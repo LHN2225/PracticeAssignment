770e6894e84f173836bf436923658ff399d44b7f
lhnhan <hoangnhan2506@gmail.com>
2023-02-17 13:45:07 +0700

    init

---
+++ PracticeAssignment/src/test/java/com/example/PracticeAssignment/PracticeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PracticeAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
